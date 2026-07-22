0a03b0248353bd27f23e734e400c1b8d44a70ff1
P.M.SALMAN KHAN <patanmohammadsalmankhan@gmail.com>
2026-07-22 22:13:52 +0530

    Solve sigmoid-numpy

--- sigmoid-numpy/sigmoid-numpy.py
+++ sigmoid-numpy/sigmoid-numpy.py
@@ -1,35 +1,25 @@
 import numpy as np
 
-def sig(x):
-    return 1 / (1 + np.exp(-x))
 def sigmoid(x):
     """
     Vectorized sigmoid function.
     """
+    def cal_sig(u):
+        return  1/ (1 + (np.exp(-u)))
     # Write code here
-    x = np.array(x)
-    return sig(x)
-    # if isinstance(x, int):
-    #     res = sig(x)
-    #     return res
-    
-    # if isinstance(x, list):
-    #     res = []
-    #     x = np.array(x)
-    #     for i in x:
-    #         res.append(sig(i))
+    if type(x) == type([]) and len(x) != 1:
+        val = list()
+        for v in x:
+            if type(v) == type([]):
+                k = []
+                for m in v:
+                    k.append(cal_sig(m))
+                val.append(k)
+                k = []
+            else:  
+                val.append(cal_sig(v))
 
-    #     return np.array(res)
-    # if any(isinstance(a, list) for a in x):
-    #     res = []
-    #     x = np.array(x)
-    #     for i in x:
-    #         inner_res = []
-    #         for j in i:
-    #             print(j)
-    #             inner_res.append(sig(j))
-
-    #         res.append(inner_res)
-
-    #     return res
-            
+        return val
+    else:
+        return cal_sig(x)
+        
